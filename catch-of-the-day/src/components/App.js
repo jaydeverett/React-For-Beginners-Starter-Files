ce4b3c66a6fc7e948b78f043f5fb57ee1f175e23
Jay Deverett <jayjdeverett@gmail.com>
2017-09-27 16:54:25 -0400

    add button to load random fish

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 
 class App extends React.Component {
@@ -9,7 +10,9 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
-    //initial state
+    this.loadSamples = this.loadSamples.bind(this);
+
+    //get initial state
     this.state = {
       fishes: {},
       order: {}
@@ -26,6 +29,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -33,7 +42,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
